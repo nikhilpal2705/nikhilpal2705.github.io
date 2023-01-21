75649d911426ec52727246ea7bcf1bd527dfeba3
nikhilpal2705 <nikhilpal2705@gmail.com>
2023-01-22 00:54:23 +0530

    cleanup and re-formatting code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,111 +1,110 @@
-particlesJS('particles-js',
-{
-  "particles": {
-    "number": {
-      "value": 160,
-      "density": {
-        "enable": true,
-        "value_area": 4000
-      }
-    },
-    "color": {
-      "value": "#ff3232"
-    },
-    "shape": {
-      "type": ["edge", "circle", "polygon", "star"],
-      "stroke": {
-        "width": 0,
-        "color": "#ff3232"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 0.8,
-        "opacity_min": 0.2,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 2,
-        "size_min": 0.3,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 600
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "window",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "bubble"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 160,
+            "density": {
+                "enable": true,
+                "value_area": 4000
+            }
+        },
+        "color": {
+            "value": "#ff3232"
+        },
+        "shape": {
+            "type": ["edge", "circle", "polygon", "star"],
+            "stroke": {
+                "width": 0,
+                "color": "#ff3232"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 0.8,
+                "opacity_min": 0.2,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 2,
+                "size_min": 0.3,
+                "sync": false
+            }
+        },
         "line_linked": {
-          "opacity": 1
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+            }
         }
-      },
-      "bubble": {
-        "distance": 250,
-        "size": 0,
-        "duration": 2,
-        "opacity": 0,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 400,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
+    },
+    "interactivity": {
+        "detect_on": "window",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 250,
+                "size": 0,
+                "duration": 2,
+                "opacity": 0,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 400,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
 }
 
 );
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,33 @@
 function showabout() {
-	$("#about_container").css("display", "inherit");
-	$("#about_container").addClass("animated slideInLeft");
-	setTimeout(function() {
-		$("#about_container").removeClass("animated slideInLeft");
-	}, 400);
+    $("#about_container").css("display", "inherit");
+    $("#about_container").addClass("animated slideInLeft");
+    setTimeout(function () {
+        $("#about_container").removeClass("animated slideInLeft");
+    }, 400);
 }
 
 function closeabout() {
-	$("#about_container").addClass("animated slideOutLeft");
-	setTimeout(function() {
-		$("#about_container").removeClass("animated slideOutLeft");
-		$("#about_container").css("display", "none");
-	}, 400);
+    $("#about_container").addClass("animated slideOutLeft");
+    setTimeout(function () {
+        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").css("display", "none");
+    }, 400);
 }
 
 function showskill() {
-	$("#skill_container").css("display", "inherit");
-	$("#skill_container").addClass("animated slideInLeft");
-	setTimeout(function() {
-		$("#skill_container").removeClass("animated slideInLeft");
-	}, 400);
+    $("#skill_container").css("display", "inherit");
+    $("#skill_container").addClass("animated slideInLeft");
+    setTimeout(function () {
+        $("#skill_container").removeClass("animated slideInLeft");
+    }, 400);
 }
 
 function closeskill() {
-	$("#skill_container").addClass("animated slideOutLeft");
-	setTimeout(function() {
-		$("#skill_container").removeClass("animated slideOutLeft");
-		$("#skill_container").css("display", "none");
-	}, 400);
+    $("#skill_container").addClass("animated slideOutLeft");
+    setTimeout(function () {
+        $("#skill_container").removeClass("animated slideOutLeft");
+        $("#skill_container").css("display", "none");
+    }, 400);
 }
 
 // function showblog() {
@@ -35,44 +35,44 @@ function closeskill() {
 // }
 
 function showwork() {
-	$("#work_container").css("display", "inherit");
-	$("#work_container").addClass("animated slideInRight");
-	setTimeout(function() {
-		$("#work_container").removeClass("animated slideInRight");
-	}, 400);
+    $("#work_container").css("display", "inherit");
+    $("#work_container").addClass("animated slideInRight");
+    setTimeout(function () {
+        $("#work_container").removeClass("animated slideInRight");
+    }, 400);
 }
 
 function closework() {
-	$("#work_container").addClass("animated slideOutRight");
-	setTimeout(function() {
-		$("#work_container").removeClass("animated slideOutRight");
-		$("#work_container").css("display", "none");
-	}, 400);
+    $("#work_container").addClass("animated slideOutRight");
+    setTimeout(function () {
+        $("#work_container").removeClass("animated slideOutRight");
+        $("#work_container").css("display", "none");
+    }, 400);
 }
 
 function showresume() {
-	window.open("https://drive.google.com/drive/folders/1ShUGu7phuU3IquAm8sgZ8kkeNOzv5U0_?usp=share_link");
+    window.open("https://drive.google.com/drive/folders/1ShUGu7phuU3IquAm8sgZ8kkeNOzv5U0_?usp=share_link");
 }
 
 function showcontact() {
-	$("#contact_container").css("display", "inherit");
-	$("#contact_container").addClass("animated slideInUp");
-	setTimeout(function() {
-		$("#contact_container").removeClass("animated slideInUp");
-	}, 400);
+    $("#contact_container").css("display", "inherit");
+    $("#contact_container").addClass("animated slideInUp");
+    setTimeout(function () {
+        $("#contact_container").removeClass("animated slideInUp");
+    }, 400);
 }
 
 function closecontact() {
-	$("#contact_container").addClass("animated slideOutDown");
-	setTimeout(function() {
-		$("#contact_container").removeClass("animated slideOutDown");
-		$("#contact_container").css("display", "none");
-	}, 400);
+    $("#contact_container").addClass("animated slideOutDown");
+    setTimeout(function () {
+        $("#contact_container").removeClass("animated slideOutDown");
+        $("#contact_container").css("display", "none");
+    }, 400);
 }
-setTimeout(function() {
-	$("#loading").addClass("animated zoomOut");
-	setTimeout(function() {
-		$("#loading").removeClass("animated zoomOut");
-		$("#loading").css("display", "none");
-	}, 500);
+setTimeout(function () {
+    $("#loading").addClass("animated zoomOut");
+    setTimeout(function () {
+        $("#loading").removeClass("animated zoomOut");
+        $("#loading").css("display", "none");
+    }, 500);
 }, 350);
