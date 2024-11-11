64509be09009d54eaf50442ce9490a38cdb30f71
nikhilpal2705 <nikhilpal2705@gmail.com>
2024-11-12 00:59:47 +0530

    added toaster

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useAOS } from '@/hooks/useAOS';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from "./routes/AppRoutes";
 import Preloader from '@/components/layouts/Preloader';
+import { ToastContainer } from 'react-toastify';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'aos/dist/aos.css';
 import 'devicon'
+import 'react-toastify/dist/ReactToastify.css';
 
 // Load this css at last
 import "@/assets/css/app.css";
@@ -21,6 +23,7 @@ const App = () => {
       <Preloader />
       <BrowserRouter>
         <AppRoutes />
+        <ToastContainer />
       </BrowserRouter>
     </>
   );
---
+++ src/components/home/ContactForm.jsx
@@ -0,0 +1,122 @@
+import emailjs from 'emailjs-com';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { useRef, useState } from "react";
+import { toast } from "react-toastify";
+
+const ContactForm = () => {
+    const form = useRef();
+    const captchaRef = useRef();
+    const [isLoading, setIsLoading] = useState(false);
+    const [captchaValue, setCaptchaValue] = useState(null); // Store the captcha value
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        // Run reCAPTCHA v3 to get the token
+        // Use size="invisible" to handle automatic captcha
+        // captchaToken = await captchaRef.current.executeAsync();
+
+        if (!captchaValue) {
+            setIsLoading(false);
+            toast.warn('Please complete the reCAPTCHA.');
+            return;
+        }
+
+        emailjs.sendForm(
+            import.meta.env.VITE_EMAILJS_SERVICE_ID,
+            import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+            form.current,
+            import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+            { 'g-recaptcha-response': captchaValue }
+        )
+            .then(() => {
+                setIsLoading(false);
+                toast.success('Your message has been sent. Thank you!');
+                form.current.reset();
+                captchaRef.current.reset(); // Reset the reCAPTCHA widget
+                setCaptchaValue(null);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                toast.error('Failed to send email. Please try again later.');
+                console.error('Error sending email:', error);
+                captchaRef.current.reset(); // Reset the reCAPTCHA widget
+                setCaptchaValue(null);
+            });
+    };
+
+    const handleCaptchaChange = (value) => {
+        if (value) {
+            toast.dismiss();
+        }
+        setCaptchaValue(value); // Store the captcha token on change
+    };
+
+
+    return (
+        <form ref={form} onSubmit={handleSubmit} className="php-email-form">
+            <div className="row gy-4">
+                <div className="col-md-6">
+                    <input
+                        type="text"
+                        name="from_name"
+                        className="form-control"
+                        placeholder="Your Name"
+                        required
+                    />
+                </div>
+
+                <div className="col-md-6">
+                    <input
+                        type="email"
+                        name="from_email"
+                        className="form-control"
+                        placeholder="Your Email"
+                        required
+                    />
+                </div>
+
+                <div className="col-md-12">
+                    <input
+                        type="text"
+                        name="subject"
+                        className="form-control"
+                        placeholder="Subject"
+                        required
+                    />
+                </div>
+
+                <div className="col-md-12">
+                    <textarea
+                        name="message"
+                        className="form-control"
+                        rows="6"
+                        placeholder="Message"
+                        required
+                    ></textarea>
+                </div>
+
+                {/* Google reCAPTCHA */}
+                <div className="col-md-12 text-center">
+                    <ReCAPTCHA
+                        ref={captchaRef}
+                        className="g-recaptcha"
+                        theme="light"
+                        style={{ display: "inline-block" }}
+                        sitekey={import.meta.env.VITE_GOOGLE_RECAPTCHA_SITE_KEY}
+                        size="normal"
+                        onChange={handleCaptchaChange} // Set captcha value on change
+                    />
+                </div>
+
+                <div className="col-md-12 text-center">
+                    <button type="submit" disabled={isLoading}>
+                        {isLoading ? (<span><span className="loading-spinner"></span>Sending...</span>) : ("Send Message")}
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default ContactForm;
